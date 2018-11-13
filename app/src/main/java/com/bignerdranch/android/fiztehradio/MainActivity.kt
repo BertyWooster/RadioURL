a6386b7b145cf1ff58fc63aeb61069e89e559eee
alekseik1 <1alekseik1@gmail.com>
2018-11-13 19:03:43 +0300

    Начальная версия Navigation Drawer

--- app/src/main/java/com/bignerdranch/android/fiztehradio/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/fiztehradio/MainActivity.kt
@@ -6,6 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import com.bignerdranch.android.fiztehradio.fragments.MainFragment
 import android.R.attr.fragment
+import android.view.View
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+
+
+
+
 
 
 
@@ -19,6 +30,32 @@ class MainActivity : AppCompatActivity() {
 
         router = Router(this, R.id.fragment_container)
         if (savedInstanceState == null) router.navigateTo(false, ::MainFragment)// переключаемся на MainFragment
+
+        val navDrawer = initDrawerItems()
+    }
+
+    fun initDrawerItems(): Drawer {
+        // Navigation Drawer
+        DrawerBuilder().withActivity(this).build()
+
+        val item1 = PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_item_home).withBadge("19")
+        val item2 = SecondaryDrawerItem().withIdentifier(2).withName(R.string.drawer_item_settings)
+
+        val result = DrawerBuilder()
+                .withActivity(this)
+                .addDrawerItems(
+                        item1,
+                        DividerDrawerItem(),
+                        item2
+                )
+                .withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                    override fun onItemClick(view: View, position: Int, drawerItem: IDrawerItem<*, *>): Boolean {
+                        // do something with the clicked item :D
+                        return false
+                    }
+                })
+                .build()
+        return result
     }
 
     override fun onBackPressed() { // Если не можем вернуться к фрагменту, закрываем активити. см класс Router
