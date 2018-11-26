4b6d61b73e32d5a69eef04f76a4646fef385cd96
Dmitry Gadecky <verevka532@gmail.com>
2018-11-26 17:16:55 +0300

    это промежуточные изменения, их описание будет в следующих коммитах.

--- app/src/main/java/com/bignerdranch/android/fiztehradio/serveces/BackgroundSoundService.kt
+++ app/src/main/java/com/bignerdranch/android/fiztehradio/serveces/BackgroundSoundService.kt
@@ -1,67 +1,104 @@
 package com.bignerdranch.android.fiztehradio.serveces
 
-import com.bignerdranch.android.fiztehradio.R.string.stop
 import android.content.Intent
 import android.os.IBinder
-import android.R.raw
-import android.app.Service
+import android.app.*
 import android.media.AudioManager
 import android.media.MediaPlayer
+import androidx.core.app.NotificationCompat
+import android.widget.Toast
+import android.media.AudioAttributes
+import android.os.Build
+import com.bignerdranch.android.fiztehradio.R
 
 
 class BackgroundSoundService : Service() {
     lateinit var player: MediaPlayer
     val mUrl: String = "http://sc2b-sjc.1.fm:8030/"
-    override fun onBind(arg0: Intent): IBinder? {
 
+
+    override fun onBind(intent: Intent): IBinder? {
         return null
     }
 
-   override fun onCreate() {
+    override fun onCreate() {
         super.onCreate()
-        player = MediaPlayer()
-        player.setDataSource(mUrl)
-        player.isLooping = true // Set looping
-        player.setVolume(100f, 100f)
+        Toast.makeText(this, " Connecting...  ", Toast.LENGTH_SHORT).show()
 
+        val notification = createNotification()
+        startForeground(1337, notification)
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        player.prepare();
 
-        // Start playing audio from http url
-        player.start();
-        return START_REDELIVER_INTENT
+        val r = object : Runnable {
+            override fun run() {
+                preparePlayer()
+                player.start();
+            }
+        }
+
+        val t = Thread(r)
+        t.start()
+        return Service.START_STICKY
     }
 
-   override fun onStart(intent: Intent, startId: Int) {
-        // TO DO
-    }
-
-    fun onUnBind(arg0: Intent): IBinder? {
-        // TO DO Auto-generated method
-        return null
-    }
-
-    fun onStop() {
-
-    }
-
-    fun onPause() {
-
-    }
-
-  override  fun onDestroy() {
-        player.stop()
+    override fun onDestroy() {
         player.release()
+        Toast.makeText(this, " Disconnecting... ", Toast.LENGTH_SHORT).show()
     }
 
-   override fun onLowMemory() {
+    fun createNotification():Notification{
+        var notification:Notification
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val mChannel = NotificationChannel("channelID", name, importance)
+            mChannel.description = descriptionText
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+
+             notification = NotificationCompat.Builder(this, "channelID")
+                    .setSmallIcon(R.drawable.notification_icon_background)
+                    .setContentTitle("RADIO ON")
+                    .setContentText("plaing")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT).build()
+        }else{
+
+             notification = NotificationCompat.Builder(this)
+                    .setContentTitle("RADIO ON")
+                    .setContentText("plaing").build()
+
+        }
+        return notification
     }
 
-    companion object {
-        private val TAG: String? = null
+    fun preparePlayer(){
+        player = MediaPlayer()
+        player.setDataSource(mUrl)
+        player.isLooping = true
+        player.setVolume(100f, 100f)
+
+        if (Build.VERSION.SDK_INT >= 21) {
+          val audioAttributes = AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA).
+                            setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build()
+            player.setAudioAttributes(audioAttributes)
+            player.prepare();
+
+        }else{
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            // This method was deprecated in API level 26, я использую альтернативу (выше), которая доступна с API 21.
+            player.prepare();
+        }
     }
+
+
+
 }
+
